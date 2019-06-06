316443ffed3aec0e2987ac60cacffdf9e2806d82
believeszw <765846560@qq.com>
2019-06-06 20:55:15 +0800

    update

--- LeetCode/src/TopInterviewQuestions/Array/Title3.h
+++ LeetCode/src/TopInterviewQuestions/Array/Title3.h
@@ -39,19 +39,31 @@ Could you do it in-place with O(1) extra space?
 
 class Title3 {
 public:
+    // 还有一个思路，三次翻转，第一次全部翻转，还有2次以k为分界线进行翻转
     void rotate(std::vector<int>& nums, int k) {
+
         int  n = nums.size();
+
         if (n == 0) return;
         if (k == 0) return;
+        if (k >= n) k = k % n;
+
         int cur = 0;
         int start = 0;
+
         for (int i = 0; i < n - 1; ++i) {
+
+            // 起始位置移动位置即本身时，起始位置向前进 1
             if ((cur + k) % n == start) {
                 start++;
                 cur = start;
                 continue;
             }
+
+            // 起始位置与移动后位置元素互换
             swap(nums[start], nums[(cur + k) % n]);
+
+            // 当前位置记录被换元素需要移动到的位置
             cur = (cur + k) % n;
         }
     }
@@ -61,6 +73,7 @@ public:
         b = a - b;
         a = a - b;
     }
+
 };
 
 
--- LeetCode/src/TopInterviewQuestions/cmake-build-debug/cmake_install.cmake
+++ LeetCode/src/TopInterviewQuestions/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/ProjectWJ/GitHub/LeetCode/LeetCode/src/TopInterviewQuestions
+# Install script for directory: C:/Users/PC-Saw/Documents/GitHub/LeetCode/LeetCode/src/TopInterviewQuestions
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/ProjectWJ/GitHub/LeetCode/LeetCode/src/TopInterviewQuestions/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/PC-Saw/Documents/GitHub/LeetCode/LeetCode/src/TopInterviewQuestions/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
