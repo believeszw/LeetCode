8b34a2ac08f08e1bdc9cfa241f19c2108eb038dc
believeszw <765846560@qq.com>
2020-03-23 22:16:45 +0800

    添加GTEST_API_

--- ByteDance/String/main.cc
+++ ByteDance/String/main.cc
@@ -5,7 +5,7 @@
 
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
+GTEST_API_ int main(int argc, char **argv) {
   std::cout << "Running main() from gtest_main.cc\n";
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
